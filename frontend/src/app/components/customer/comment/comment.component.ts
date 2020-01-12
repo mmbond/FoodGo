43fa99d28ae990d8b683e27ffba1cfd39e498259
Marko Milic <marko.milic.16@singimail.rs>
2020-01-12 16:24:37 +0100

    comment profile table view

--- frontend/src/app/components/customer/comment/comment.component.ts
+++ frontend/src/app/components/customer/comment/comment.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Order } from 'src/app/models/order.model';
+import { HistoryService } from 'src/app/services/history.service';
+import { ErrorHelper } from 'src/app/utilities/ErrorHelper';
 
 @Component({
   selector: 'app-comment',
@@ -8,16 +11,26 @@ import { Router } from '@angular/router';
 })
 export class CommentComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  orders: Array<Order>;
+  error: any;
+  
+  constructor(private router: Router, private _historyService: HistoryService) { }
 
   ngOnInit() {
+    this._fetchOrders(10);
   }
 
-  private orders() {
-    this.router.navigate(['customer/']);
+  private _fetchOrders(limit: number = 1) {
+    this._historyService.getOrders(limit).toPromise()
+      .then(response => this.orders = response)
+      .catch(error => this.error = ErrorHelper.generateErrorObj(error));
+  }
+
+  private goToOrders() {
+    this.router.navigate(['customer/orders']);
   }
 
   private showComments() : boolean {
-    return false;
+    return this.orders!=undefined;
   }
 }
