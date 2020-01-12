4befb3516deb48c81e3b26ff92689300bf94ba54
Marko Milic <marko.milic.16@singimail.rs>
2020-01-12 17:33:29 +0100

    customer profile page with functionality of edit

--- frontend/src/app/components/customer/profile/profile.component.ts
+++ frontend/src/app/components/customer/profile/profile.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CustomerProfile } from 'src/app/models/customer-profile.model';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { AuthenticationService } from 'src/app/services/authentication.service';
+import { CustomerService } from 'src/app/services/customer.service';
 import { first } from 'rxjs/operators';
 
 @Component({
@@ -18,22 +18,22 @@ export class ProfileComponent implements OnInit {
   submitted = false;
   returnUrl: string;
   hide = true;
+  showForm = false;
   mobNumberPattern = "^((\\+381)|0)+[0-9]{8,9}$";
   addressPattern = "^([A-z]+\\s)+([1-9]|[1-9][0-9]{1,2})[a-z]{0,1}$";
   
-  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute, private router: Router, private authenticationService: AuthenticationService) {
+  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute, private router: Router, private customerService: CustomerService) {
     this.customer = JSON.parse(localStorage.getItem('customer'));
   }
 
   ngOnInit() {
     this.profileForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
+      email: [this.customer.email, [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]],
       confirmPassword: ['', [Validators.required, Validators.minLength(8)]],
-      name: ['', Validators.required],
-      lastname: ['', Validators.required],
-      address: ['', Validators.required],
-      phone: ['', Validators.required]
+      name: [this.customer.firstName, Validators.required],
+      lastname: [this.customer.lastName, Validators.required],
+      phone: [this.customer.phone, Validators.required]
     }, { validator: passwordsMustMatch('password', 'confirmPassword') });
 
     // get return url from route parameters or default to '/'
@@ -52,12 +52,33 @@ export class ProfileComponent implements OnInit {
     }
 
     this.customer.email = this.f.email.value;
-    //this.customer.password = this.f.password.value;
     this.customer.firstName = this.f.name.value;
     this.customer.lastName = this.f.lastname.value;
     this.customer.phone = this.f.phone.value;
-    this.customer.address = this.f.address.value;
     this.loading = true;
+    console.log(this.customer);
+    this.customerService.updateCustomer(this.customer)
+    .pipe(first())
+    .subscribe(
+      data => {
+        this.showForm = false;
+        this.loading = false;
+        this.profileForm.reset({
+          email: this.customer.email,
+          name: this.customer.firstName,
+          lastname: this.customer.lastName,
+          phone: this.customer.phone
+        });
+        (this as{submitted:boolean}).submitted = false;
+      },
+      error => {
+        this.showForm = true;
+      });
+      
+  }
+
+  private editProfile() : boolean {
+    return this.showForm;
   }
 }
 
--- frontend/src/app/services/customer.service.ts
+++ frontend/src/app/services/customer.service.ts
@@ -14,35 +14,38 @@ export class CustomerService {
   private _apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
-  
+
   // Post edit customerProfile
   updateCustomer(_customerProfile: CustomerProfile) {
     // TODO videti da li mozda da backend vrati neku potvrdu
     localStorage.setItem('customer', JSON.stringify(_customerProfile));
-    this.http.post(`${this._apiUrl}/profile/edit`, _customerProfile);
+    return this.http.post<boolean>(`${this._apiUrl}/profile/edit`, _customerProfile)
+      .pipe(map(value => {
+        return value;
+      }));
   }
 
   // get Customer
   getCustomer(limit: number = 1) {
-      const requestUrl = `${this._apiUrl}/profile/get`;
-      return this.http.get<CustomerProfile>(requestUrl)
-          .pipe(response => {
-              return response;
-          }),
-          catchError(error => {
-              return throwError(error);
-          });
+    const requestUrl = `${this._apiUrl}/profile/get`;
+    return this.http.get<CustomerProfile>(requestUrl)
+      .pipe(response => {
+        return response;
+      }),
+      catchError(error => {
+        return throwError(error);
+      });
   }
 
-    // get Customer addresses
-    getCustomerAddresses(limit: number = 1) {
-      const requestUrl = `${this._apiUrl}/profile/getAddresses`;
-      return this.http.get<ProfileAddressList>(requestUrl)
-          .pipe(map(response => {
-              return response;
-          }),
-          catchError(error => {
-              return throwError(error);
-          }));
+  // get Customer addresses
+  getCustomerAddresses(limit: number = 1) {
+    const requestUrl = `${this._apiUrl}/profile/getAddresses`;
+    return this.http.get<ProfileAddressList>(requestUrl)
+      .pipe(map(response => {
+        return response;
+      }),
+        catchError(error => {
+          return throwError(error);
+        }));
   }
 }
